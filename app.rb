dfd07ffd264dbc481cc3f2e95a3e1964592faf25
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-12-17 20:46:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,14 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    puts params
-    @team_name = params["name"]
-    @coach = params["coach"]
-    @pg = params["pg"]
-    @sg = params["sg"]
-    @sf = params["sf"]
-    @pf = params["pf"]
-    @ct = params["ct"]
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @ct = params[:ct]
     erb :team
   end
 end
